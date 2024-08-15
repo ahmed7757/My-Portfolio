2342545822eb64c611e649e926ce1910777b1373
Ahmed Gomaa <ag8496001@gmail.com>
2024-08-16 02:33:14 +0300

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,10 +12,10 @@ const Navbar = () => {
                 <img src={Logo} alt="Logo" className=" size-24" />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-3xl text-zinc-50">
-                <FaLinkedin />
-                <FaGithub />
-                <FaSquareXTwitter />
-                <FaInstagram />
+                <a target="_blank" href="https://www.linkedin.com/in/ahmed-gomaa-499b55254/"><FaLinkedin /></a>
+                <a target="_blank" href="https://github.com/ahmed7757"><FaGithub /></a>
+                <a target="_blank" href="https://x.com/Ag8496001Ahmed"><FaSquareXTwitter /></a>
+                <a target="_blank" href="https://www.instagram.com/ahmedg7700/"><FaInstagram /></a>
             </div>
         </nav>
     )
